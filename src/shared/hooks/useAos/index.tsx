1c90091a82d4cb9e1dd2b6b2e579b0f33d2ad3e5
K3ndev <racelisjk@gmail.com>
2023-08-27 19:41:32 +0800

    chore: add husky

--- src/shared/hooks/useAos/index.tsx
+++ src/shared/hooks/useAos/index.tsx
@@ -4,7 +4,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 export function useAos() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     AOS.init();
   }, []);
